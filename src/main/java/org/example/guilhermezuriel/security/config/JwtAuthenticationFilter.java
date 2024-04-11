3eafbbb1a07c590b4c71af41a26f6e346666e9b1
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-04-11 12:05:31 -0300

    JwtAuthenticationFilter

--- src/main/java/org/example/guilhermezuriel/security/config/JwtAuthenticationFilter.java
+++ src/main/java/org/example/guilhermezuriel/security/config/JwtAuthenticationFilter.java
@@ -1,2 +1,36 @@
-package org.example.guilhermezuriel.security.config;public class JwtAuthenticationFilter {
+package org.example.guilhermezuriel.security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if(authHeader == null || !authHeader.startsWith("Bearer")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);// todo extract the UserEmail from JWT TOKEN;
+    }
 }
