ca349f73c11776d7c9e74ee781cc615220dc41ec
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-04-11 12:45:15 -0300

    Getting SignIn Keys

--- src/main/java/org/example/guilhermezuriel/security/config/JwtService.java
+++ src/main/java/org/example/guilhermezuriel/security/config/JwtService.java
@@ -1,10 +1,30 @@
 package org.example.guilhermezuriel.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    private final String SECRET_KEY = "9F6AF6BDF3FF8C532F793DF7B938E";
     public String extractUsername(String token) {
         return null;
     }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder().setSigningKey(getSignIngKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignIngKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
 }
